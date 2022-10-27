a5e83b8a39f2691881619531889c9bdec6359b95
Matthew Loney <116615618+mtlforlife72@users.noreply.github.com>
2022-10-27 15:12:13 -0700

    Update streamlit_app.py
    Add local function  get_fruit_load_list() for pulling FRUIT_LOAD_LIST in snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ def get_fruityfice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+# snowflake related function
+def get_fruit_load_list():
+  with my_cur = my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -43,16 +49,16 @@ try:
     
 except URLError as e:
   streamlit.error()
-  
-streamlit.stop()
 
 # Working with snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fuit load list contains:")
-streamlit.dataframe(my_data_rows)
+streamlit.text("The fruit load list contains:")
+# add a button to load the fruit  
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 
 # Allow the user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add to the list?')
