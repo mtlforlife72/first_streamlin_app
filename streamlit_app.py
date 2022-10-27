ae7429cbacaf0dfb342c5068c60fae46dfbcdbf8
Matthew Loney <116615618+mtlforlife72@users.noreply.github.com>
2022-10-27 15:03:17 -0700

    Update streamlit_app.py
    Trouble shooting errors line 41, in <module> back_from_function = get_fruityvice_data(fruit_choice) NameError: name 'get_fruityvice_data' is not defined

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-# create the repeatable code block (called a function)
-def get_fruityfice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-  
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -31,6 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+# create the repeatable code block (called a function)
+def get_fruityfice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 # New Section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try: 
@@ -38,7 +38,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_fruityvice_data(fruit_choice)    
+    back_from_function = get_fruityfice_data(fruit_choice)    
     streamlit.dataframe(back_from_function)
     
 except URLError as e:
